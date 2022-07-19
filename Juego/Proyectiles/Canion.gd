b31d44dadc3991397c9081bd3b8670df7376daed
Angelo Nicolas Wolhein <angelonicolasw@gmail.com>
2022-07-19 19:08:19 -0300

    fix sonido

--- Juego/Proyectiles/Canion.gd
+++ Juego/Proyectiles/Canion.gd
@@ -39,9 +39,8 @@ func disparar() -> void:
 	esta_enfriado = false
 	disparo_sfx.play()
 	timer_enfriamiento.start()
-	
+	disparo_sfx.play()
 	for punto_disparo in puntos_disparo:
-		disparo_sfx.play()
 		var new_proyectil:Proyectil = proyectil.instance()
 		new_proyectil.crear(
 			punto_disparo.global_position,
